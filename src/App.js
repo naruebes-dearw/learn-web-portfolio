2821786a55584eaeeb0613424e5c6f68bfdf7822
naruebes-dearw <naruebes.phudeth@gmail.com>
2021-10-26 16:50:56 +0700

    fixed the cannot-get-darkMode bug

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
+import { useContext } from "react";
 import About from "./components/about/About";
 import Contact from "./components/contact/Contact";
 import Intro from "./components/intro/Intro";
 import ProductList from "./components/productList/ProductList";
 import Toggle from "./components/toggle/Toggle";
-import { useContext } from "react";
 import { ThemeContext } from "./context";
 
 const App = () => {
   const theme = useContext(ThemeContext);
-  console.log(theme.state.darkMode);
-  // const darkMode = theme.state.darkMode;
-  return <div style={{ backgroundColor: "red", color: "white" }}>
-    <Toggle />
-    <Intro />
-    <About />
-    <ProductList />
-    <Contact />
-  </div>;
+  const darkMode = theme.state.darkMode;
+  return (
+    <div
+      style={{
+        backgroundColor: darkMode && "#222",
+        color: darkMode && "#fff"
+      }}
+    >
+      <Toggle />
+      <Intro />
+      <About />
+      <ProductList />
+      <Contact />
+    </div>
+  );
 };
 
 export default App;
