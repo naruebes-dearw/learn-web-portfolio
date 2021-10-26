6a533fa867a4eb2ac77feb1fd235889906141401
naruebes-dearw <naruebes.phudeth@gmail.com>
2021-10-26 17:11:59 +0700

    add dark mode

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,10 +2,14 @@ import "./contact.css";
 import Phone from "../../img/phone.png";
 import Email from "../../img/email.png";
 import Address from "../../img/address.png";
-import { useRef, useState } from "react";
+import { useContext, useRef, useState } from "react";
 import emailjs from 'emailjs-com';
+import { ThemeContext } from "../../context";
 
 const Contact = () => {
+  const theme = useContext(ThemeContext);
+  const darkMode = theme.state.darkMode;
+
   const formRef = useRef()
   const [done, setDone] = useState(false);
 
@@ -56,10 +60,10 @@ const Contact = () => {
             <b>What's your story</b> Get in touch. Always available for freelancing if the right project comes along. me.
           </p>
           <form ref={formRef} onSubmit={handleSubmit}>
-            <input type="text" placeholder="Name" name="user_name" />
-            <input type="text" placeholder="Subject" name="user_subject" />
-            <input type="text" placeholder="Email" name="user_email" />
-            <textarea rows="5" placeholder="Message" name="message" />
+            <input style={{ backgroundColor: darkMode && "#333" }} type="text" placeholder="Name" name="user_name" />
+            <input style={{ backgroundColor: darkMode && "#333" }} type="text" placeholder="Subject" name="user_subject" />
+            <input style={{ backgroundColor: darkMode && "#333" }} type="text" placeholder="Email" name="user_email" />
+            <textarea style={{ backgroundColor: darkMode && "#333" }} rows="5" placeholder="Message" name="message" />
             <button>Submit</button>
             {done && "Thanks you..."}
           </form>
--- src/components/toggle/Toggle.jsx
+++ src/components/toggle/Toggle.jsx
@@ -1,15 +1,26 @@
 import "./toggle.css";
 import Sun from "../../img/sun.png";
 import Moon from "../../img/moon.png";
+import { ThemeContext } from "../../context";
+import { useContext } from "react";
 
-const toggle = () => {
+const Toggle = () => {
+  const theme = useContext(ThemeContext)
+  const handleClick = () => {
+    theme.dispatch({
+      type: "TOGGLE"
+    })
+  }
   return (
-    <div className="t">
+    <div className="t" onClick={handleClick}>
       <img src={Sun} alt="" className="t-icon" />
       <img src={Moon} alt="" className="t-icon" />
-      <div className="t-button"></div>
+      <div
+        className="t-button"
+        style={{ left: theme.state.darkMode ? "0" : "25px" }}
+      ></div>
     </div>
   )
 }
 
-export default toggle
+export default Toggle
