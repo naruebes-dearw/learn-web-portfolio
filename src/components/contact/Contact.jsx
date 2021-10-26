e67b6e85a39ca451c868e9a652f0fd1138c3ce02
naruebes-dearw <naruebes.phudeth@gmail.com>
2021-10-26 14:55:14 +0700

    add contact component

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,72 @@
+import "./contact.css";
+import Phone from "../../img/phone.png";
+import Email from "../../img/email.png";
+import Address from "../../img/address.png";
+import { useRef, useState } from "react";
+import emailjs from 'emailjs-com';
+
+const Contact = () => {
+  const formRef = useRef()
+  const [done, setDone] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    emailjs.sendForm(
+      'service_bevcokc',
+      'template_z4fjgze',
+      formRef.current,
+      'user_IwYml9sBhvBhxA59ujyM6'
+    )
+      .then((result) => {
+        console.log(result.text);
+        setDone(true);
+      }, (error) => {
+        console.log(error.text);
+      });
+  }
+  return (
+    <div className="c">
+      <div className="c-bg"></div>
+      <div className="c-wrapper">
+        <div className="c-left">
+          <h1 className="c-title">Let's discuss your project</h1>
+          <div className="c-info">
+            <div className="c-info-item">
+              <img
+                src={Phone} alt="" className="c-icon"
+              />
+              +1 1234 556 75
+            </div>
+            <div className="c-info-item">
+              <img
+                src={Email} alt="" className="c-icon"
+              />
+              naruebes.phudeth@email.com
+            </div>
+            <div className="c-info-item">
+              <img
+                src={Address} alt="" className="c-icon"
+              />
+              245 King Street, Touterie Victoria 8520 Australia
+            </div>
+          </div>
+        </div>
+        <div className="c-right">
+          <p className="c-desc">
+            <b>What's your story</b> Get in touch. Always available for freelancing if the right project comes along. me.
+          </p>
+          <form ref={formRef} onSubmit={handleSubmit}>
+            <input type="text" placeholder="Name" name="user_name" />
+            <input type="text" placeholder="Subject" name="user_subject" />
+            <input type="text" placeholder="Email" name="user_email" />
+            <textarea rows="5" placeholder="Message" name="message" />
+            <button>Submit</button>
+            {done && "Thanks you..."}
+          </form>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Contact
